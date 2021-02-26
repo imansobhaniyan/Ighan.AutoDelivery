c57864e722326d60734831d381654f1ba7648f74
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2021-02-26 10:58:11 +0330

    some changes for readability

--- Ighan.AutoDelivery.ConsoleDemo/Program.cs
+++ Ighan.AutoDelivery.ConsoleDemo/Program.cs
@@ -16,33 +16,34 @@ namespace Ighan.AutoDelivery.ConsoleDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please specify config postfix:");
+
+            var configFilePostFix = Console.ReadLine();
+
             var dt = DateTime.Now;
 
-            var solution = JsonSerializer.Deserialize<Solution>(File.ReadAllText(Environment.CurrentDirectory + "\\config.json"));
+            var solution = JsonSerializer.Deserialize<Solution>
+                (File.ReadAllText(Environment.CurrentDirectory + $"\\config-{configFilePostFix}.json"));
 
-            if (args != null && args.Length > 0)
-                solution.Projects = solution.Projects.Where(f => args.Any(arg => f.Name == arg)).ToList();
-
-            if (args == null || !args.Contains("monitoring"))
-                solution.Projects = solution.Projects.Where(f => f.Name != "monitoring").ToList();
+            FilterProjects(args, solution);
 
             GitRepositoryPuller.Pull(solution.RepositoryPath);
 
             foreach (var project in solution.Projects)
             {
-                DotNetBuilder.Release(Path.Combine(solution.RepositoryPath, project.ProjectPath), Path.Combine(solution.DestinationPath, project.SiteName, "Publish"));
+                DotNetBuilder.Release(Path.Combine(solution.RepositoryPath, project.ProjectPath), GetPath(solution, project, PathType.Publish));
 
                 IISWebSiteManager.Stop(project.SiteName);
 
-                DirectoryFullGraphCopier.Copy(Path.Combine(solution.DestinationPath, project.SiteName, "WebSite"), Path.Combine(solution.DestinationPath, project.SiteName, "BackUp"));
+                DirectoryFullGraphCopier.Copy(GetPath(solution, project, PathType.WebSite), GetPath(solution, project, PathType.BackUp));
 
-                DirectoryFullGraphCopier.Copy(Path.Combine(solution.DestinationPath, project.SiteName, "Publish"), Path.Combine(solution.DestinationPath, project.SiteName, "WebSite"));
+                DirectoryFullGraphCopier.Copy(GetPath(solution, project, PathType.Publish), GetPath(solution, project, PathType.WebSite));
 
                 foreach (var solutionSetting in solution.SolutionSettings)
-                    FileContentReplacer.ReplaceByKey(Path.Combine(solution.DestinationPath, project.SiteName, "WebSite", solutionSetting.FileName), solutionSetting.Key, solutionSetting.NewValue);
+                    FileContentReplacer.ReplaceByKey(GetPath(solution, project, PathType.WebSite, solutionSetting.FileName), solutionSetting.Key, solutionSetting.NewValue);
 
                 foreach (var projectSetting in project.ProjectSettings)
-                    FileContentReplacer.ReplaceByKey(Path.Combine(solution.DestinationPath, project.SiteName, "WebSite", projectSetting.FileName), projectSetting.Key, projectSetting.NewValue);
+                    FileContentReplacer.ReplaceByKey(GetPath(solution, project, PathType.WebSite, projectSetting.FileName), projectSetting.Key, projectSetting.NewValue);
 
                 IISWebSiteManager.Start(project.SiteName);
             }
@@ -52,6 +53,32 @@ namespace Ighan.AutoDelivery.ConsoleDemo
             Console.WriteLine($"Finished in: {period}");
 
             Console.ReadLine();
+        }
+
+        public enum PathType
+        {
+            WebSite,
+            BackUp,
+            Publish,
+        }
+
+        public static string GetPath(Solution solution, Project project, PathType pathType, string fileName = null)
+        {
+            var result = Path.Combine(solution.DestinationPath, project.SiteName, pathType.ToString());
+            
+            if (!string.IsNullOrWhiteSpace(fileName))
+                result = Path.Combine(result, fileName);
+
+            return result;
+        }
+
+        private static void FilterProjects(string[] args, Solution solution)
+        {
+            if (args != null && args.Length > 0)
+                solution.Projects = solution.Projects.Where(f => args.Any(arg => f.Name == arg)).ToList();
+
+            if (args == null || !args.Contains("monitoring"))
+                solution.Projects = solution.Projects.Where(f => f.Name != "monitoring").ToList();
         }
     }
 }
